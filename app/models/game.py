3024fb5f26ea71121f0ac9316ddcd8271cf76a41
BBYBOI <zeygermanleo@gmail.com>
2026-09-19 14:56:51 +0500

    create gameorm

---
+++ app/models/game.py
@@ -0,0 +1,22 @@
+from sqlalchemy import ForeignKey
+from models.base import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+class GamesOrm(Base):
+    __tablename__ = 'games'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    game_title: Mapped[str]
+    description: Mapped[str]
+    release_year: Mapped[int]
+
+    platform_id: Mapped[int] = mapped_column(ForeignKey('platforms.id'))
+
+    platform: Mapped['PlatformsOrm'] = relationship(
+        back_populates='games'
+    )
+
+    genres: Mapped[list['GenresOrm']] = relationship(
+        secondary='game_genre',
+        back_populates='games'
+    )
