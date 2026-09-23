d796489a759aa0e0cf6704db3daff49615e792b8
BBYBOI <zeygermanleo@gmail.com>
2026-09-23 19:48:38 +0500

    get platform by id

--- app/routers/platform.py
+++ app/routers/platform.py
@@ -1,3 +1,6 @@
+from fastapi import HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
 from app.routers.game import router
 from app.schemas.platform import ResponsePlatform, CreatePlatform, ResponsePlatformWithGames
 from app.models.platform import PlatformsOrm
@@ -13,4 +16,11 @@ async def create_platform(session: SessionDep, platform = CreatePlatform):
 
 @router.get('/platforms/{platform_id}', summary='Получить платформу по ID', response_model=ResponsePlatformWithGames)
 async def get_platform_by_id(session: SessionDep, platform_id: int):
-    pass
+    platform = await session.scalar(
+        select(PlatformsOrm)
+        .where(PlatformsOrm.id == platform_id)
+        .options(selectinload(PlatformsOrm.games))
+    )
+    if platform is None:
+        raise HTTPException(status_code=404, detail='Платформа не найдена!')
+    return platform
